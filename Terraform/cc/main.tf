b2df2a1f8b1f44546c104df12b26598da0568d67
Krisz <rybarkristik@gmail.com>
2024-02-16 22:40:56 +0100

    temp commit

---
+++ Terraform/cc/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.91.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "mtc-rg" {
+  name     = "mtc-resources"
+  location = "northeurope"
+  tags = {
+    enviroment = "dev"
+  }
+}
